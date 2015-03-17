104bac1e9402c8ee685f0c159f2e658f782c0a59
samadammeek <sam.meek@gmail.com>
2015-03-17 12:15:59 +0000

    Finished NumberOfNeighbour* processes

--- WEB-INF/classes/pillar/authoritativedata/NumberOfNeighbourNamedFeatures.java
+++ WEB-INF/classes/pillar/authoritativedata/NumberOfNeighbourNamedFeatures.java
@@ -1,14 +1,19 @@
 package pillar.authoritativedata;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.store.ReprojectingFeatureCollection;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.referencing.CRS;
 import org.n52.wps.io.data.IData;
 import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
@@ -21,6 +26,7 @@ import org.n52.wps.server.ExceptionReport;
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.PropertyType;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CRSAuthorityFactory;
@@ -35,6 +41,7 @@ import com.vividsolutions.jts.geom.Geometry;
 
 public class NumberOfNeighbourNamedFeatures extends AbstractAlgorithm{
 	
+	Logger LOG = Logger.getLogger(NumberOfNeighbourNamedFeatures.class);
 	private final String inputObservations = "inputObservations";
 	private final String inputAuthoritativeData = "inputAuthoritativeData";
 	private final String inputDistance = "inputDistance";
@@ -75,9 +82,6 @@ public class NumberOfNeighbourNamedFeatures extends AbstractAlgorithm{
 		if(identifier.equalsIgnoreCase("qual_result")){
 			return GTVectorDataBinding.class;
 		}
-		if(identifier.equalsIgnoreCase("metadata")){
-			return GenericFileDataBinding.class;
-		}
 		return null;
 	}
 
@@ -93,57 +97,93 @@ public class NumberOfNeighbourNamedFeatures extends AbstractAlgorithm{
 		List featList = inputData.get("featureName");
 		
 		
-		FeatureCollection obsFcW = ((GTVectorDataBinding) obsList.get(0)).getPayload();
-		FeatureCollection authFcW = ((GTVectorDataBinding) authList.get(0)).getPayload();
+		FeatureCollection obsFc = ((GTVectorDataBinding) obsList.get(0)).getPayload();
+		FeatureCollection authFc = ((GTVectorDataBinding) authList.get(0)).getPayload();
 		double dist = ((LiteralDoubleBinding)distList.get(0)).getPayload();
 		int minNum = ((LiteralIntBinding)minList.get(0)).getPayload();
 		String fieldName = ((LiteralStringBinding)fieldList.get(0)).getPayload();
 		String featureN = ((LiteralStringBinding)featList.get(0)).getPayload();
 		
-		
 		ArrayList<SimpleFeature> resultFeatures = new ArrayList<SimpleFeature>();
-		
-		CoordinateReferenceSystem sourceCRS = null;
-		
-		CoordinateReferenceSystem projectCRS = null;
-		
-		CRSAuthorityFactory   factory = CRS.getAuthorityFactory(true);
-		try {
-			sourceCRS = factory.createCoordinateReferenceSystem("EPSG:4326");
-			projectCRS = factory.createCoordinateReferenceSystem("EPSG:27700");
-		} catch (NoSuchAuthorityCodeException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (FactoryException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		FeatureCollection obsFc = new ReprojectingFeatureCollection(obsFcW, projectCRS);
-		FeatureCollection authFc = new ReprojectingFeatureCollection(authFcW, projectCRS);
+		ArrayList<SimpleFeature> returnFeatures = new ArrayList<SimpleFeature>();
 		
 		SimpleFeatureIterator obsIt = (SimpleFeatureIterator) obsFc.features();
 		SimpleFeatureIterator authIt = (SimpleFeatureIterator) authFc.features();
 		
+		SimpleFeatureIterator sfi = (SimpleFeatureIterator) obsFc.features();
 		SimpleFeatureType fType = null;
+		SimpleFeature tempPropFeature = sfi.next();
+		CoordinateReferenceSystem inputObsCrs = obsFc.getSchema().getCoordinateReferenceSystem();
 		
-		while (obsIt.hasNext()){
-			SimpleFeature tempFeature = obsIt.next();
+		Collection<Property> obsProp = tempPropFeature.getProperties();
+		
+		SimpleFeatureTypeBuilder resultTypeBuilder = new SimpleFeatureTypeBuilder();
+		resultTypeBuilder.setName("typeBuilder");
+		resultTypeBuilder.setCRS(inputObsCrs);
+		
+		Iterator<Property> pItObs = obsProp.iterator();
+		
+		sfi.close();
+		while (pItObs.hasNext()==true){
+			
+			try{
+				
+			Property tempProp = pItObs.next();
+			PropertyType type = tempProp.getDescriptor().getType();
+			String name = type.getName().getLocalPart();
+			Class<String> valueClass = (Class<String>)tempProp.getType().getBinding();
+			
+			resultTypeBuilder.add(name, valueClass);
+			
+			//LOG.warn ("Obs property " + name + " " + valueClass + " " +type.toString());
+			}
+			catch (Exception e){
+				LOG.error("property error " + e);
+			}
+		}
+		
+		//add DQ_Field
+		
+		resultTypeBuilder.add("DQ_SA_NamedF", Double.class);
+		SimpleFeatureType typeF = resultTypeBuilder.buildFeatureType();
+		//LOG.warn("Get Spatial Accuracy Feature Type " + typeF.toString());
+		
+		SimpleFeatureBuilder resultFeatureBuilder = new SimpleFeatureBuilder(typeF);
+		
+		obsIt.close();
+		
+		SimpleFeatureIterator obsIt2 = (SimpleFeatureIterator) obsFc.features();
+		
+		while (obsIt2.hasNext()){
+			SimpleFeature tempFeature = obsIt2.next();
 			
 			fType = tempFeature.getType();
-		
+			
+			//LOG.warn("fieldName " + fieldName + " featureName " + featureN + " tempFeature Type " + fType);
+			//LOG.warn("TableFeatureName " + tempFeature.getProperty(fieldName).getValue());
+			
 			String tempFeatureName = (String) tempFeature.getProperty(fieldName).getValue();
 			
-			
-			
 			Geometry geom = (Geometry) tempFeature.getDefaultGeometry();
+			for (Property obsProperty : tempFeature.getProperties()){
+
+				
+				String name = obsProperty.getName().toString();
+				Object value = obsProperty.getValue();
+				
+				resultFeatureBuilder.set(name, value);
+				
+			}
 			
 			Geometry bufferGeom = geom.buffer(dist);
 			
-			int count = 0;
+			SimpleFeatureIterator authIt2 = (SimpleFeatureIterator) authFc.features();
 			
-			while (authIt.hasNext() && count <= minNum){
-				SimpleFeature tempAuth = authIt.next();
+			int count = 0;
+			int within = 0;
+			
+			while (authIt2.hasNext() && count <= minNum && tempFeatureName.equalsIgnoreCase(featureN)){
+				SimpleFeature tempAuth = authIt2.next();
 				Geometry tempGeom = (Geometry) tempAuth.getDefaultGeometry();
 				
 				String authProperty = (String) tempAuth.getProperty(fieldName).getValue();
@@ -154,31 +194,45 @@ public class NumberOfNeighbourNamedFeatures extends AbstractAlgorithm{
 					count++;
 					
 					if(count >= minNum){
-						resultFeatures.add(tempFeature);
+						within = 1;
+		
 					}
 					
 				}
+				
+			}
+			LOG.warn("HERE " + within);
+			resultFeatureBuilder.set("DQ_SA_NamedF", within);
+			
+			SimpleFeature result = resultFeatureBuilder.buildFeature(tempFeature.getID());
+			result.setDefaultGeometry(geom);
+			LOG.warn("HERE " + result);
+			returnFeatures.add(result);
+			
+			if(within == 1){
+				resultFeatures.add(result);
 			}
 			
 			
+			authIt2.close();
 			
 		}
-		
-		
-		FeatureCollection obsFcR = new ReprojectingFeatureCollection(obsFc, sourceCRS);
+		obsIt2.close();	
 		
 		ListFeatureCollection qualResult = new ListFeatureCollection(fType, resultFeatures);
+		ListFeatureCollection returns = new ListFeatureCollection(fType, returnFeatures);
 		
-		FeatureCollection resultF = new ReprojectingFeatureCollection(qualResult, sourceCRS);
+		LOG.warn("HERE 2 " + qualResult.size() + " " + returns.size());
 		
 		Map <String, IData> results = new HashMap<String, IData>();
-		results.put("qual_result", new GTVectorDataBinding(resultF));
-		results.put("result", new GTVectorDataBinding(obsFcR));
-		results.put("metadata", new GenericFileDataBinding(null));
+		results.put("qual_result", new GTVectorDataBinding(qualResult));
+		results.put("result", new GTVectorDataBinding(returns));
+		
 		
 		
 		return results;
 	}
+	
 	@Override
 	public List<String> getErrors() {
 		// TODO Auto-generated method stub
--- WEB-INF/classes/pillar/authoritativedata/NumberOfNeighbourSimilarFeatures.java
+++ WEB-INF/classes/pillar/authoritativedata/NumberOfNeighbourSimilarFeatures.java
@@ -1,14 +1,19 @@
 package pillar.authoritativedata;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.store.ReprojectingFeatureCollection;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.referencing.CRS;
 import org.n52.wps.io.data.IData;
 import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
@@ -21,6 +26,7 @@ import org.n52.wps.server.ExceptionReport;
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.PropertyType;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CRSAuthorityFactory;
@@ -34,6 +40,8 @@ import com.vividsolutions.jts.geom.Geometry;
 //metadata ThematicAccuracy
 
 public class NumberOfNeighbourSimilarFeatures extends AbstractAlgorithm{
+	
+	Logger LOG = Logger.getLogger(NumberOfNeighbourSimilarFeatures.class);
 	
 	private final String inputObservations = "inputObservations";
 	private final String inputAuthoritativeData = "inputAuthoritativeData";
@@ -89,91 +97,145 @@ public class NumberOfNeighbourSimilarFeatures extends AbstractAlgorithm{
 		List fieldList = inputData.get("fieldName");
 		
 		
-		FeatureCollection obsFcW = ((GTVectorDataBinding) obsList.get(0)).getPayload();
-		FeatureCollection authFcW = ((GTVectorDataBinding) authList.get(0)).getPayload();
+		
+		FeatureCollection obsFc = ((GTVectorDataBinding) obsList.get(0)).getPayload();
+		FeatureCollection authFc = ((GTVectorDataBinding) authList.get(0)).getPayload();
 		double dist = ((LiteralDoubleBinding)distList.get(0)).getPayload();
 		int minNum = ((LiteralIntBinding)minList.get(0)).getPayload();
 		String fieldName = ((LiteralStringBinding)fieldList.get(0)).getPayload();
 		
 		
 		ArrayList<SimpleFeature> resultFeatures = new ArrayList<SimpleFeature>();
-		
-		CoordinateReferenceSystem sourceCRS = null;
-		
-		CoordinateReferenceSystem projectCRS = null;
-		
-		CRSAuthorityFactory   factory = CRS.getAuthorityFactory(true);
-		try {
-			sourceCRS = factory.createCoordinateReferenceSystem("EPSG:4326");
-			projectCRS = factory.createCoordinateReferenceSystem("EPSG:27700");
-		} catch (NoSuchAuthorityCodeException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (FactoryException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		FeatureCollection obsFc = new ReprojectingFeatureCollection(obsFcW, projectCRS);
-		FeatureCollection authFc = new ReprojectingFeatureCollection(authFcW, projectCRS);
+		ArrayList<SimpleFeature> returnFeatures = new ArrayList<SimpleFeature>();
 		
 		SimpleFeatureIterator obsIt = (SimpleFeatureIterator) obsFc.features();
 		SimpleFeatureIterator authIt = (SimpleFeatureIterator) authFc.features();
 		
+		SimpleFeatureIterator sfi = (SimpleFeatureIterator) obsFc.features();
 		SimpleFeatureType fType = null;
+		SimpleFeature tempPropFeature = sfi.next();
+		CoordinateReferenceSystem inputObsCrs = obsFc.getSchema().getCoordinateReferenceSystem();
 		
-		while (obsIt.hasNext()){
-			SimpleFeature tempFeature = obsIt.next();
+		Collection<Property> obsProp = tempPropFeature.getProperties();
+		
+		SimpleFeatureTypeBuilder resultTypeBuilder = new SimpleFeatureTypeBuilder();
+		resultTypeBuilder.setName("typeBuilder");
+		resultTypeBuilder.setCRS(inputObsCrs);
+		
+		Iterator<Property> pItObs = obsProp.iterator();
+		
+		sfi.close();
+		while (pItObs.hasNext()==true){
+			
+			try{
+				
+			Property tempProp = pItObs.next();
+			PropertyType type = tempProp.getDescriptor().getType();
+			String name = type.getName().getLocalPart();
+			Class<String> valueClass = (Class<String>)tempProp.getType().getBinding();
+			
+			resultTypeBuilder.add(name, valueClass);
+			
+			//LOG.warn ("Obs property " + name + " " + valueClass + " " +type.toString());
+			}
+			catch (Exception e){
+				LOG.error("property error " + e);
+			}
+		}
+		
+		//add DQ_Field
+		
+		resultTypeBuilder.add("DQ_SA_SimilarF", Double.class);
+		SimpleFeatureType typeF = resultTypeBuilder.buildFeatureType();
+		//LOG.warn("Get Spatial Accuracy Feature Type " + typeF.toString());
+		
+		SimpleFeatureBuilder resultFeatureBuilder = new SimpleFeatureBuilder(typeF);
+		
+		obsIt.close();
+		
+		SimpleFeatureIterator obsIt2 = (SimpleFeatureIterator) obsFc.features();
+		
+		while (obsIt2.hasNext()){
+			SimpleFeature tempFeature = obsIt2.next();
 			
 			fType = tempFeature.getType();
-		
+			
+			//LOG.warn("fieldName " + fieldName + " featureName " + featureN + " tempFeature Type " + fType);
+			//LOG.warn("TableFeatureName " + tempFeature.getProperty(fieldName).getValue());
+			
 			String tempFeatureName = (String) tempFeature.getProperty(fieldName).getValue();
 			
-			
-			
 			Geometry geom = (Geometry) tempFeature.getDefaultGeometry();
+			for (Property obsProperty : tempFeature.getProperties()){
+
+				
+				String name = obsProperty.getName().toString();
+				Object value = obsProperty.getValue();
+				
+				resultFeatureBuilder.set(name, value);
+				
+			}
 			
 			Geometry bufferGeom = geom.buffer(dist);
 			
+			SimpleFeatureIterator authIt2 = (SimpleFeatureIterator) authFc.features();
+		
 			int count = 0;
+			int within = 0;
 			
-			while (authIt.hasNext() && count <= minNum){
-				SimpleFeature tempAuth = authIt.next();
+			while (authIt2.hasNext() && count <= minNum){
+				SimpleFeature tempAuth = authIt2.next();
+				String featureN = (String) tempAuth.getProperty(fieldName).getValue();
+				
 				Geometry tempGeom = (Geometry) tempAuth.getDefaultGeometry();
 				
 				String authProperty = (String) tempAuth.getProperty(fieldName).getValue();
 				
 				
-				if (tempGeom.within(bufferGeom) && authProperty.equalsIgnoreCase(tempFeatureName)){
+				if (tempGeom.within(bufferGeom) && authProperty.equalsIgnoreCase(featureN)){
 					
 					count++;
 					
 					if(count >= minNum){
-						resultFeatures.add(tempFeature);
+						within = 1;
+		
 					}
 					
 				}
+				
+			}
+			LOG.warn("HERE " + within);
+			resultFeatureBuilder.set("DQ_SA_SimilarF", within);
+			
+			SimpleFeature result = resultFeatureBuilder.buildFeature(tempFeature.getID());
+			result.setDefaultGeometry(geom);
+			LOG.warn("HERE " + result);
+			returnFeatures.add(result);
+			
+			if(within == 1){
+				resultFeatures.add(result);
 			}
 			
 			
+			authIt2.close();
 			
 		}
-		
-		
-		FeatureCollection obsFcR = new ReprojectingFeatureCollection(obsFc, sourceCRS);
+		obsIt2.close();	
 		
 		ListFeatureCollection qualResult = new ListFeatureCollection(fType, resultFeatures);
+		ListFeatureCollection returns = new ListFeatureCollection(fType, returnFeatures);
 		
-		FeatureCollection resultF = new ReprojectingFeatureCollection(qualResult, sourceCRS);
+		LOG.warn("HERE 2 " + qualResult.size() + " " + returns.size());
 		
 		Map <String, IData> results = new HashMap<String, IData>();
-		results.put("qual_result", new GTVectorDataBinding(resultF));
-		results.put("result", new GTVectorDataBinding(obsFcR));
-		results.put("metadata", new GenericFileDataBinding(null));
+		results.put("qual_result", new GTVectorDataBinding(qualResult));
+		results.put("result", new GTVectorDataBinding(returns));
+		
 		
 		
 		return results;
 	}
+	
 	@Override
 	public List<String> getErrors() {
 		// TODO Auto-generated method stub
